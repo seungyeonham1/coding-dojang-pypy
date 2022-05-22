a8ab27efd8c4b533a1e918b5cace8a59a8a4aeb6
minwoomint <hwangsin2728@gmail.com>
2022-05-22 18:38:29 +0900

    파이썬 스크립트 연습문제, 심사문제 추가

---
+++ unit06/judge_average.py
@@ -0,0 +1,7 @@
+
+a = int(input('국어 점수를 입력하세요 >'))
+b = int(input('수학 점수를 입력하세요 >'))
+c = int(input('과학 점수를 입력하세요 >'))
+d = int(input('영어 점수를 입력하세요 >'))
+
+print(int((a+b+c+d)/4))
---
+++ unit06/practice_input_spilt.py
@@ -0,0 +1,3 @@
+x, y, z = map(int, input('숫자 3개를 입력하세요 > ').split())
+
+print(x+y+z)

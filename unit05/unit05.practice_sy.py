b8723587ad5b034c71adc1d728739716ba958438
minwoomint <hwangsin2728@gmail.com>
2022-05-22 16:46:33 +0900

    파이썬 연습 스크리트 업데이트

---
+++ unit05/unit05.practice_sy.py
@@ -0,0 +1,22 @@
+# 파이썬의 숫자 자료형에는 int, float, complex
+
+
+# a = 10
+# b = 1.5
+
+# print(type(a))
+# print(type(b))
+
+# print(1+1)
+# print(1-2)
+# print(3*3)
+# print(11//3)
+
+# print(15%6)
+# print(2**5)
+
+
+# print(int(3.3))
+
+
+
---
+++ unit06/unit06.practice_sy.py
@@ -0,0 +1,18 @@
+# a = 10
+# b = 20
+
+# a += 20
+
+# print(a+b)
+
+
+
+# input_number1 = int(input())
+# input_number2 = int(input())
+
+# print(input_number1+input_number2)
+
+
+
+a, b = map(int, input().split())
+print(a+b)
